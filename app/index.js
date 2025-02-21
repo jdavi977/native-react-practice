4b3d54372248bec87d67ad72fdcc61aac7f3699a
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-20 23:20:39 -0700

    UI Design start

--- app/index.js
+++ app/index.js
@@ -1,23 +1,36 @@
-import { Text, View } from "react-native";
-import SwipeDeck from "../components/SwipeDeck";
-
+import { Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { petData as petDataArray } from "../utils/data.js";
+import Card from "../components/Card.js";
+import { StatusBar } from 'expo-status-bar';
 
 export default function Index() {
+  const [petData, setPetData] = useState(petDataArray);
+
+  useEffect(() => {
+    if(!petData.length){
+      setPetData(petData)
+    }
+  }, [petData.length])
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View>
-        <Text>Test</Text>
-        <SwipeDeck />
-      </View>
-      <View>
-        <Text>Hello</Text>
-      </View>
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "white" }}>
+      <StatusBar hidden={true} />
+      {
+        petData.map(({name, age, breed, image}, index) => {
+          const isFirst = index === 0;
+          return (
+            <Card
+              key={name}
+              name={name}
+              age={age}
+              breed={breed}
+              image={image}
+              isFirst={isFirst}
+            />
+          )
+        })
+      }
     </View>
   );
 }
---
+++ utils/data.js
@@ -0,0 +1,23 @@
+export const petData = [
+    {
+        id: 1,
+        name: "Nori",
+        breed: "Bunny",
+        age: 3,
+        image: require("../assets/images/adaptive-icon.png")
+    },
+    {
+        id: 2,
+        name: "Mila",
+        breed: "Bunnt",
+        age: 4,
+        image: require("../assets/images/favicon.png")
+    },
+    {
+        id: 3,
+        name: "Coco",
+        breed: "Hamster",
+        age: 1,
+        image: require("../assets/images/icon.png")
+    }
+]
